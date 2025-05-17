fc450134c70f20a7452fcfb26bce26c7c01f1c16
cau20232907 <hs20232907@cau.ac.kr>
2025-05-18 03:03:41 +0900

    init 코드 삭제

--- src/main/java/com/t3f4/zerowaste/global/config/DBTestdataConfig.java
+++ src/main/java/com/t3f4/zerowaste/global/config/DBTestdataConfig.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Component
+//@Component
 public class DBTestdataConfig {
     DBTestdataConfig(MemberRepository memberRepository,
                      MissionRepository missionRepository,
